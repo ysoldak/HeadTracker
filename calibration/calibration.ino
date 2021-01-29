4b5889a7f7c8bb0d2ea81803155b9bde11b5e050
Yurii Soldak <yurii.soldak@nordnet.se>
2021-01-29 02:20:44 +0100

    Calibration: use original Arduino_LSM9DS1 library, rather than v2 Some tweaks to the library needed though: rates shall be adjusted

--- calibration/calibration.ino
+++ calibration/calibration.ino
@@ -4,26 +4,27 @@
 // Please, set these two constants for your location
 // https://en.wikipedia.org/wiki/Earth%27s_magnetic_field
 
-const float_t EarthMagnetStrength = 51.5;  //= µT (Stockholm)
-const float_t EarthMagnetInclination = 73; //=deg (Stockholm)
+const float_t EarthMagnetStrength    = 51.5; //= µT (Stockholm)
+const float_t EarthMagnetInclination = 73;   //=deg (Stockholm)
 
 // Following constants shall be good enough, tweak if feeling adventurous
 
 const uint8_t NofCalibrationSamples = 10;  // Number of samples collected for each axis considered enough to start calibration
-const float_t AccelCriterion = 0.1;        // Accelerometer detection tolerance
+const float_t AccelCriterion        = 0.1; // Accelerometer axis detection tolerance
 
-const uint8_t AccelODRindex  = 5;  // 0:off, 1:10Hz, 2:50Hz, 3:119Hz, 4:238Hz, 5:476Hz, (6:952Hz=na)
-const uint8_t AccelFSindex   = 3;  // 0: ±2g ; 1: ±24g ; 2: ±4g ; 3: ±8g
-const uint8_t MagnetODRindex = 8;  // (0..8)->{0.625,1.25,2.5,5.0,10,20,40,80,400}Hz
-const uint8_t MagnetFSindex  = 0;  // 0=±400.0; 1=±800.0; 2=±1200.0 , 3=±1600.0  (µT)
 
-const float_t StartAccelSlopes[3]   = {1, 1, 1};
-const float_t StartAccelOffsets[3]  = {0, 0, 0};
-const float_t StartMagnetSlopes[3]  = {1, 1, 1};
-const float_t StartMagnetOffsets[3] = {0, 0, 0};
+// Expected value of magnetic field strength along each axis
+const float_t MagTarget = EarthMagnetStrength * cos((90-EarthMagnetInclination) * PI/180);
 
 const char xyz[3]= {'X','Y','Z'};
-const float_t MagTarget = EarthMagnetStrength * cos((90-EarthMagnetInclination) * PI/180);
+
+float_t imuCalibration[4][3] = {
+  {1, 1, 1},
+  {0, 0, 0},
+  {1, 1, 1},
+  {0, 0, 0}
+};
+
 
 float_t calSamplesMin[3] = {0, 0, 0};
 float_t calSamplesMax[3] = {0, 0, 0};
@@ -34,20 +35,9 @@ float_t magSamplesMax[3] = {0, 0, 0};
 void setup() {
   Serial.begin(115200);
   while (!Serial);
-  pinMode(LED_BUILTIN,OUTPUT);
   delay(10);
-  if (!IMU.begin()) { Serial.println(F("Failed to initialize IMU!")); while (1);  }
-
-  IMU.setAccelFS(AccelFSindex);
-  IMU.setAccelODR(AccelODRindex);
-  IMU.setAccelOffset(0, 0, 0);
-  IMU.setAccelSlope (1, 1, 1);
-
-  IMU.setMagnetODR(MagnetODRindex);
-  IMU.setMagnetFS(MagnetFSindex);
-  IMU.setMagnetOffset(0, 0, 0);
-  IMU.setMagnetSlope (1, 1, 1);
-
+  IMU.begin();
+  IMU.setOneShotMode();
 }
 
 void loop() {
@@ -60,23 +50,20 @@ void calibrateMenu() {
     Serial.println(F("Calibrate Accelerometer and Magnetometer\n"));
 
     Serial.println(F(" Run calibration a couple of times for the values to settle\n"));
-    Serial.println(F(" and errors [shown in square brackets] be less or around 1\n"));
+    Serial.println(F(" and errors [shown in square brackets] be in single digits\n"));
 
     Serial.println(F(" (Enter)  Start calibration"));
 
     Serial.println("\n\n");
-    Serial.print(F("  // Accelerometer code\n"));
-    Serial.print(F("  IMU.setAccelFS(")); Serial.print(AccelFSindex);Serial.print(F(");\n"));
-    Serial.print(F("  IMU.setAccelODR("));Serial.print(AccelODRindex);Serial.println(");");
-    printSetParam ("  IMU.setAccelOffset",IMU.accelOffset); Serial.println();
-    printSetParam ("  IMU.setAccelSlope ",IMU.accelSlope);
-
-    Serial.println("\n\n");
-    Serial.print(F("  // Magnetometer code\n"));
-    Serial.print(F("  IMU.setMagnetFS("));Serial.print(MagnetFSindex);Serial.println(");");
-    Serial.print(F("  IMU.setMagnetODR("));Serial.print(MagnetODRindex);Serial.println(");");
-    printSetParam ("  IMU.setMagnetOffset",IMU.magnetOffset); Serial.println();
-    printSetParam ("  IMU.setMagnetSlope ",IMU.magnetSlope);
+    for (int i = 0; i < 4; i++) {
+      Serial.print("{");
+      for (int j = 0; j < 3; j++) {
+        Serial.print(imuCalibration[i][j]); if (j < 2) Serial.print(", ");
+      }
+      Serial.print("}");
+      if (i < 3) Serial.print(",");
+      Serial.println();
+    }
     Serial.println(F("\n\n"));
 
     readChar();
@@ -111,14 +98,14 @@ void calibrate() {
   while(!enough) {
     float_t acc[3];
     float_t mag[3];
-    readAvg(100, acc, mag);
+    readAvg(10, acc, mag);
 
     bool shallPrintStatus = false;
     for (int i = 0; i<3; i++) {
       float_t a = acc[i], b = acc[(i+1)%3], c = acc[(i+2)%3];
       if (abs(a)>max(abs(b),abs(c))) {
         if (sqrt(b*b+c*c)/a<AccelCriterion) {
-          // Serial.print(". ");
+          Serial.print(". ");
           if (a < 0) {
             magSamplesMin[i] = min(mag[i], magSamplesMin[i]);
             calSamplesMin[i]++;
@@ -132,27 +119,28 @@ void calibrate() {
       }
     }
     if (shallPrintStatus) {
+      Serial.println();
       for (int i=0; i<3; i++) {
-        Serial.print(xyz[i]);Serial.print("- ");
+        Serial.print(xyz[i]);Serial.print("-");
         if (calSamplesMin[i] >= NofCalibrationSamples) {
-          Serial.print(" OK ["); Serial.print(MagTarget+magSamplesMin[i]); Serial.print("] ");
+          Serial.print("OK [ "); Serial.print(MagTarget+magSamplesMin[i]); Serial.print(" ]   ");
         } else {
-          Serial.print(" no data   ");
+          Serial.print(" -- [ --.-- ]   ");
         }
-        Serial.print(xyz[i]);Serial.print("+ ");
+        Serial.print(xyz[i]);Serial.print("+");
         if (calSamplesMax[i] >= NofCalibrationSamples) {
-          Serial.print(" OK ["); Serial.print(magSamplesMax[i]-MagTarget); Serial.print("] ");
+          Serial.print("OK [ "); Serial.print(magSamplesMax[i]-MagTarget); Serial.print(" ]   ");
         } else {
-          Serial.print(" no data   ");
+          Serial.print(" -- [ --.-- ]   ");
         }
       }
       Serial.println();
     }
 
-    // float_t cosValue = vector_dot(mag, acc) / (sqrt(vector_dot(mag, mag)) * sqrt(vector_dot(acc, acc)));
-    // float_t angle = 90 - acos (cosValue) * 180.0 / PI;
-    // printFormatted("Acc: %f,%f,%f    Mag: %f,%f,%f    Angle: %f", acc[0], acc[1], acc[2], mag[0], mag[1], mag[2], angle);
-    // printFormatted("%f, %f, %f / %f, %f, %f  |  %f, %f, %f / %f, %f, %f\n", magSamplesMin[0], magSamplesMin[1], magSamplesMin[2], magSamplesMax[0], magSamplesMax[1], magSamplesMax[2], calSamplesMin[0], calSamplesMin[1], calSamplesMin[2], calSamplesMax[0], calSamplesMax[1], calSamplesMax[2]);
+    //  float_t cosValue = vector_dot(mag, acc) / (sqrt(vector_dot(mag, mag)) * sqrt(vector_dot(acc, acc)));
+    //  float_t angle = 90 - acos (cosValue) * 180.0 / PI;
+    //  printFormatted("Acc: %f,%f,%f    Mag: %f,%f,%f    Angle: %f", acc[0], acc[1], acc[2], mag[0], mag[1], mag[2], angle);
+    //  printFormatted("%f, %f, %f / %f, %f, %f  |  %f, %f, %f / %f, %f, %f\n", magSamplesMin[0], magSamplesMin[1], magSamplesMin[2], magSamplesMax[0], magSamplesMax[1], magSamplesMax[2], calSamplesMin[0], calSamplesMin[1], calSamplesMin[2], calSamplesMax[0], calSamplesMax[1], calSamplesMax[2]);
 
     enough = true;
     for (int i = 0; i<3; i++) {
@@ -164,15 +152,18 @@ void calibrate() {
 
   }
 
-  float_t offsetX = IMU.magnetOffset[0]+(magSamplesMax[0]+magSamplesMin[0])/2/IMU.magnetSlope[0];
-  float_t offsetY = IMU.magnetOffset[1]+(magSamplesMax[1]+magSamplesMin[1])/2/IMU.magnetSlope[1];
-  float_t offsetZ = IMU.magnetOffset[2]+(magSamplesMax[2]+magSamplesMin[2])/2/IMU.magnetSlope[2];
-  float_t slopeX  = IMU.magnetSlope[0]*(2*MagTarget)/(magSamplesMax[0]-magSamplesMin[0]);
-  float_t slopeY  = IMU.magnetSlope[1]*(2*MagTarget)/(magSamplesMax[1]-magSamplesMin[1]);
-  float_t slopeZ  = IMU.magnetSlope[2]*(2*MagTarget)/(magSamplesMax[2]-magSamplesMin[2]);
-  IMU.setMagnetOffset(offsetX, offsetY, offsetZ);
-  IMU.setMagnetSlope (slopeX, slopeY, slopeZ);
-  IMU.setMagnetODR(MagnetODRindex);
+  float_t offsetX = imuCalibration[3][0]+(magSamplesMax[0]+magSamplesMin[0])/2/imuCalibration[2][0];
+  float_t offsetY = imuCalibration[3][1]+(magSamplesMax[1]+magSamplesMin[1])/2/imuCalibration[2][1];
+  float_t offsetZ = imuCalibration[3][2]+(magSamplesMax[2]+magSamplesMin[2])/2/imuCalibration[2][2];
+  float_t slopeX  = imuCalibration[2][0]*(2*MagTarget)/(magSamplesMax[0]-magSamplesMin[0]);
+  float_t slopeY  = imuCalibration[2][1]*(2*MagTarget)/(magSamplesMax[1]-magSamplesMin[1]);
+  float_t slopeZ  = imuCalibration[2][2]*(2*MagTarget)/(magSamplesMax[2]-magSamplesMin[2]);
+  imuCalibration[2][0] = slopeX;
+  imuCalibration[2][1] = slopeY;
+  imuCalibration[2][2] = slopeZ;
+  imuCalibration[3][0] = offsetX;
+  imuCalibration[3][1] = offsetY;
+  imuCalibration[3][2] = offsetZ;
 
   for (int i = 0; i<3; i++) {
     magSamplesMin[i] = 0;
@@ -180,7 +171,6 @@ void calibrate() {
     calSamplesMin[i] = 0;
     calSamplesMax[i] = 0;
   }
-
 
 }
 
@@ -194,11 +184,15 @@ void readAvg(uint8_t N, float_t a[3], float_t m[3]) {
 
   float_t x, y, z;
   for (uint8_t i = 0; i < N; i++) {
-    while (!IMU.magnetAvailable() || !IMU.accelAvailable());
-    IMU.readAccel(x, y, z);
-    a[0] += x; a[1] -= y; a[2] -= z; // substract on Y and Z to align acc vector with mag vector
-    IMU.readMagnet(x, y, z);
-    m[0] += x; m[1] += y; m[2] += z;
+    while (!IMU.magneticFieldAvailable() || !IMU.accelerationAvailable());
+    IMU.readAcceleration(x, y, z);
+    a[0] += x;
+    a[1] -= y;
+    a[2] -= z; // substract on Y and Z to align acc vector with mag vector
+    IMU.readMagneticField(x, y, z);
+    m[0] += imuCalibration[2][0] * (x - imuCalibration[3][0]);
+    m[1] += imuCalibration[2][1] * (y - imuCalibration[3][1]);
+    m[2] += imuCalibration[2][2] * (z - imuCalibration[3][2]);
   }
 
   for (uint8_t i = 0; i < 3; i++) {
