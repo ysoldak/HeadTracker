68146115aa70ff99a00f077d122723313d6b312f
Yurii Soldak <yurii.soldak@nordnet.se>
2021-01-30 22:46:32 +0100

    use "#pragma once"

--- main/imu.h
+++ main/imu.h
@@ -1,5 +1,4 @@
-#ifndef IMU_H
-#define IMU_H
+#pragma once
 
 #include <Arduino_LSM9DS1.h>
 
@@ -11,5 +10,3 @@ void imuRead(int N, float_t acc[3], float_t mag[3]);
 void imuReadAcceleration(float_t out[3]);
 void imuReadMagneticField(float_t out[3]);
 void imuReadMagneticField(float_t out[3], float_t variance[3]);
-
-#endif
--- main/para.h
+++ main/para.h
@@ -1,9 +1,6 @@
-#ifndef PARA_H
-#define PARA_H
+#pragma once
 
 #include <ArduinoBLE.h>
 
 void paraSetup();
 void paraSend(const uint16_t channels[8]);
-
-#endif
