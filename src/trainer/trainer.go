7bab5973d1d0387a8d84f652a9f389f6289966d8
Yurii Soldak <ysoldak@gmail.com>
2025-11-22 00:27:35 +0100

    feat: Reset orientation via Bluetooth link (Write 'R' to 0xAFF2) (#80)

--- src/trainer/trainer.go
+++ src/trainer/trainer.go
@@ -3,9 +3,12 @@ package trainer
 type Trainer interface {
 	Configure(name string)
 	Start()
+	Channels() []uint16
+	SetChannel(num int, v uint16)
+
+	// Bluetooth specific
 	Update()
 	Paired() bool
 	Address() string
-	Channels() []uint16
-	SetChannel(num int, v uint16)
+	ResetRequested() bool
 }
